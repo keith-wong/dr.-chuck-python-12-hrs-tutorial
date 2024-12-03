fe3f07dd1907693bbd13b1b10067ca6a51377606
KeithWong <kwongchiu66@gmail.com>
2024-12-03 13:16:26 +0800

    {add new file conditional.py}

---
+++ conditional.py
@@ -0,0 +1,33 @@
+x = 5
+if x > 2:
+    print ('bigger than 2')
+    print ('still bigger')
+
+print ('Done')
+
+# below code is for loop to print 1 to 5. 
+# if number greater than 2 print Bigger than 2 and finally print 'All Done'
+
+
+
+for i in range (5):
+    print (i)
+
+    if i > 2 :
+        print ('bigger than 2')
+print ('done with i')
+
+print ('all done')
+
+# the following code is nested if statement
+
+x = 50
+
+if x > 1:
+    print ('more than one')
+    if x < 100 :
+        print ('less than 100')
+
+print ('all done!')
+
+
